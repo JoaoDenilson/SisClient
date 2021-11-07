7ca25725683c52e0a3cea32dcd5e7717ee0e5ef1
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2021-11-07 20:31:18 -0300

    First Commit

---
+++ SisClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SisClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
